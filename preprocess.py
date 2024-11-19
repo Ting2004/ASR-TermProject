2fe016e339e1f4c372b1612c46d7518cb3e24c0b
Ting Chen <88258612+Ting2004@users.noreply.github.com>
2024-11-18 20:57:09 -0500

    Update preprocess.py
    update for noisy dataset

--- preprocess.py
+++ preprocess.py
@@ -10,9 +10,9 @@ out_noise = "librispeech_noise"
 
 def process_agegender():
 
-    ds_train = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="train")
-    ds_vali = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="validation")
-    ds_test = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="test")
+    ds_train = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="train", trust_remote_code=True)
+    ds_vali = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="validation", trust_remote_code=True)
+    ds_test = load_dataset("mozilla-foundation/common_voice_16_0", "en", split="test", trust_remote_code=True)
 
     ds_train = ds_train.remove_columns(["up_votes", "down_votes", "accent", "locale", "segment", "variant"])
     ds_train = ds_train.filter(lambda row: row["gender"] != "" and row["age"] != "")
@@ -38,11 +38,14 @@ def process_noise():
     # total size would be probably 200 hrs (clean and noisy)
     # need to reconsider what is clean and what is noisy
 
-    ds_pub_test = load_dataset("distil-whisper/librispeech_asr-noise", "test-pub-noise", split="40")
-    ds_pub_vali = load_dataset("distil-whisper/librispeech_asr-noise", "validation-pub-noise", split="40")
+    # the number for split indicate how much noise there is
+    # smaller means more noisy
 
-    ds_clean_test = load_dataset("distil-whisper/librispeech_asr-noise", "test-pub-noise", split="0")
-    ds_clean_vali = load_dataset("distil-whisper/librispeech_asr-noise", "validation-pub-noise", split="0")
+    ds_pub_test = load_dataset("distil-whisper/librispeech_asr-noise", "test-pub-noise", split="0", trust_remote_code=True)
+    ds_pub_vali = load_dataset("distil-whisper/librispeech_asr-noise", "validation-pub-noise", split="0", trust_remote_code=True)
+
+    ds_clean_test = load_dataset("distil-whisper/librispeech_asr-noise", "test-pub-noise", split="40", trust_remote_code=True)
+    ds_clean_vali = load_dataset("distil-whisper/librispeech_asr-noise", "validation-pub-noise", split="40", trust_remote_code=True)
 
 
     true_column = ["true"] * len(ds_pub_test)
@@ -58,13 +61,20 @@ def process_noise():
     ds_clean_vali = ds_pub_vali.add_column("noise", false_column)
 
 
-    test = concatenate_datasets([ds_clean_test, ds_pub_test])
-    vali = concatenate_datasets([ds_clean_vali, ds_pub_vali])
+    noisy = concatenate_datasets([ds_pub_test, ds_pub_vali])
+    clean = concatenate_datasets([ds_clean_test, ds_clean_vali])
 
+    # regenerate train test split
+    noisy = noisy.train_test_split(test=0.3)
+    clean = clean.train_test_split(test=0.3)
+
+    test = concatenate_datasets([clean["test"], noisy["test"]])
+    train = concatenate_datasets([clean["train"], noisy["train"]])
     ds = DatasetDict({
-        "validation": vali,
+        "train": train,
         "test": test
     })
     ds.push_to_hub(out_noise, private=False)
 
 
+process_agegender()
